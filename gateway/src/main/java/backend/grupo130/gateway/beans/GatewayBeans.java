4e3c76c90250a70dc205fff7b4e46fffc8dbd278
JulianTs0 <juliantorresssr@gmail.com>
2025-11-02 15:48:59 -0300

    se renombro un paquete

--- gateway/src/main/java/backend/grupo130/gateway/beans/GatewayBeans.java
+++ gateway/src/main/java/backend/grupo130/gateway/beans/GatewayBeans.java
@@ -1,4 +1,4 @@
-package backend.grupo130.gateway.Beans;
+package backend.grupo130.gateway.beans;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
--- gateway/src/main/java/backend/grupo130/gateway/beans/WebClientConfig.java
+++ gateway/src/main/java/backend/grupo130/gateway/beans/WebClientConfig.java
@@ -1,4 +1,4 @@
-package backend.grupo130.gateway.Beans;
+package backend.grupo130.gateway.beans;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
